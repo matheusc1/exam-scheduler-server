cd0ee82b74c323d548022032fa42c9b05f2b56d7
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2024-11-02 23:14:09 -0300

    feat: create database tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,112 @@
+import {
+  pgTable,
+  text,
+  integer,
+  date,
+  pgEnum,
+  time,
+  index,
+  timestamp,
+} from 'drizzle-orm/pg-core'
 import { createId } from '@paralleldrive/cuid2'
-import { pgTable, text } from 'drizzle-orm/pg-core'
 
-export const test = pgTable('test', {
+const rolesEnum = pgEnum('roles', ['admin', 'coordinator'])
+const examEnum = pgEnum('type', ['mandatory', 'substitute'])
+
+export const adminUser = pgTable('admin_user', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  email: text('email').notNull().unique(),
+  password: text('password').notNull(),
+  role: rolesEnum(),
+})
+
+export const student = pgTable('student', {
+  ra: text('ra').notNull().unique().primaryKey(),
+  name: text('name').notNull(),
+  email: text('email').notNull().unique(),
+  password: text('password').notNull(),
+  birthDate: date('birth_date', { mode: 'date' }).notNull(),
+  supportCenter: text('support-center_id')
+    .notNull()
+    .references(() => supportCenter.id),
+})
+
+export const period = pgTable('period', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  startDate: date('start_date', { mode: 'date' }).notNull(),
+  endDate: date('end_date', { mode: 'date' }).notNull(),
+})
+
+export const discipline = pgTable('discipline', {
   id: text('id')
     .primaryKey()
     .$defaultFn(() => createId()),
   name: text('name').notNull(),
+})
+
+export const enrollment = pgTable('enrollment', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  studentId: text('student_id')
+    .notNull()
+    .references(() => student.ra),
+  disciplineId: text('discipline_id')
+    .notNull()
+    .references(() => discipline.id),
+  periodId: text('period_id')
+    .notNull()
+    .references(() => period.id),
+})
+
+export const exam = pgTable(
+  'exam',
+  {
+    id: text('id')
+      .primaryKey()
+      .$defaultFn(() => createId()),
+    enrollmentId: text('enrollment_id')
+      .notNull()
+      .references(() => enrollment.id),
+    type: examEnum(),
+    scheduledDate: timestamp('scheduled_date', { withTimezone: true }),
+  },
+  t => [index('exam_enrollment_type_idx').on(t.enrollmentId, t.type)]
+)
+
+export const supportCenter = pgTable('support_center', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  name: text('name').notNull().unique(),
+  numberOfComputers: integer('number_of_computers').notNull(),
+})
+
+export const supportCenterOperatingHours = pgTable(
+  'support_center_operating_hours',
+  {
+    id: text('id')
+      .primaryKey()
+      .$defaultFn(() => createId()),
+    supportCenter: text('support_center_id').references(() => supportCenter.id),
+    weekDay: integer('week_day').notNull(),
+    openTime: time('open_time').notNull(),
+    closeTime: time('close_time').notNull(),
+  }
+)
+
+export const availableSlots = pgTable('available_slots', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  date: date('date', { mode: 'date' }).notNull(),
+  time: time('time').notNull(),
+  availableSlots: integer('available_slots').notNull(),
+  supportCenter: text('support_center_id')
+    .notNull()
+    .references(() => supportCenter.id),
 })

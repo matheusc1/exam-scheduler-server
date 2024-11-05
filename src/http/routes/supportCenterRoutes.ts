731437259f2fc33ecc5bb8742c3a26c2605520d5
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2024-11-05 20:38:04 -0300

    feat: add support centers routes

---
+++ src/http/routes/supportCenterRoutes.ts
@@ -0,0 +1,64 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod'
+import { createSupportCenter } from '../../functions/support-center/create-support-center'
+import { deleteSupportCenter } from '../../functions/support-center/delete-support-center'
+import { getSupportCenters } from '../../functions/support-center/get-support-center'
+import { updateSupportCenter } from '../../functions/support-center/update-support-center'
+
+export const supportCenterRoutes: FastifyPluginAsyncZod = async app => {
+  app.post(
+    '/support-center',
+    {
+      schema: {
+        body: z.object({
+          name: z.string(),
+          numberOfComputers: z.number(),
+        }),
+      },
+    },
+    async request => {
+      const { name, numberOfComputers } = request.body
+      await createSupportCenter({ name, numberOfComputers })
+    }
+  )
+
+  app.delete(
+    '/support-center/:id',
+    {
+      schema: {
+        params: z.object({
+          id: z.string(),
+        }),
+      },
+    },
+    async request => {
+      const { id } = request.params
+      await deleteSupportCenter({ id })
+    }
+  )
+
+  app.get('/support-center', async () => {
+    const { supportCenters } = await getSupportCenters()
+    return { supportCenters }
+  })
+
+  app.put(
+    '/support-center/:id',
+    {
+      schema: {
+        params: z.object({
+          id: z.string(),
+        }),
+        body: z.object({
+          name: z.string(),
+          numberOfComputers: z.number(),
+        }),
+      },
+    },
+    async request => {
+      const { id } = request.params
+      const { name, numberOfComputers } = request.body
+      await updateSupportCenter({ id, name, numberOfComputers })
+    }
+  )
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,25 @@
 import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import fastifyCors from '@fastify/cors'
 
-const app = fastify()
+import { supportCenterRoutes } from './routes/supportCenterRoutes'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  // origin: /localhost\:5173/,
+  origin: '*',
+  credentials: true,
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(supportCenterRoutes)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running')

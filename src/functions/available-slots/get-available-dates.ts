7c0be97b667817fc81f78302ccb99ac38c514d7c
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2024-11-07 20:40:11 -0300

    feat: add function to get available dates

---
+++ src/functions/available-slots/get-available-dates.ts
@@ -0,0 +1,22 @@
+import { eq } from 'drizzle-orm'
+import { db } from '../../db'
+import { availableSlots } from '../../db/schema'
+
+interface GetAvailableDatesRequest {
+  supportCenterId: string
+}
+
+export async function getAvailableDates({
+  supportCenterId,
+}: GetAvailableDatesRequest) {
+  const records = await db
+    .select()
+    .from(availableSlots)
+    .where(eq(availableSlots.supportCenter, supportCenterId))
+
+  const uniqueDates = [
+    ...new Set(records.map(record => record.date.toISOString().split('T')[0])),
+  ]
+
+  return uniqueDates
+}
